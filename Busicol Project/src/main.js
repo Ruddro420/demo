09e7c25552ce81cc6d214915fb3bcc15780e67e8
Ruddro420 <63606099+Ruddro420@users.noreply.github.com>
2020-04-27 09:48:58 -0700

    Add files via upload

---
+++ Busicol Project/src/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $('.counter').counterUp(
+        {
+            delay: 2,
+            time: 1000
+        }
+    );
+
+
+    $('#t').t({
+        repeat:true,
+        speed:40,
+        pause_on_click:true,
+        beep:false,
+    });
+      
+
+
+
+
+
+
+
+
+      });
